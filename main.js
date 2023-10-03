530ffe452177afcc515fea02bb389574d8ed408d
MariaGabi123 <105298537+MariaGabi123@users.noreply.github.com>
2023-10-03 08:37:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla.pom').onclick = tocaSomPom();
+
+
+function tocaSomClap () {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+function tocaSomTim () {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim').onclick = tocaSomClap;
+
+function tocaSomPuff () {
+    document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+function tocaSomSplash () {
+    document.querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+function tocaSomToim () {
+    document.querySelector('#som_tecla_Toim').play();
+}
+
+document.querySelector('.tecla_Toim').onclick = tocaSomToim;
+
+function tocaSomPsh () {
+    document.querySelector('#som_tecla_Psh').play();
+}
+
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+function tocaSomTic () {
+    document.querySelector('#som_tecla_tic').play();
+}
+
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+function tocaSomTom () {
+    document.querySelector('#som_tecla_Tom').play();
+}
+
+document.querySelector('.tecla_tom').onclick = tocaSomTom;
+
+
+
